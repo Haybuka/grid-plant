4b45cdb137499fce37afc2e130635a9c026721a6
Haybuka <paschal.nduleo@gmail.com>
2020-05-25 15:10:12 +0100

    Added a bit of functonality to the button element

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const toggle = document.querySelector('nav #toggler');
 const flip = document.querySelector('nav ul');
+const btn = document.querySelectorAll('button');
+
+const buttons = Array.from(btn);
 toggle.addEventListener('click', function () {
-    flip.classList.toggle('open')
+    flip.classList.toggle('open');
+})
+
+buttons.forEach((n) => {
+    n.addEventListener('mouseover', function () {
+        this.style.color = 'white'
+        this.style.backgroundColor = "black"
+    });
+    n.addEventListener('mouseout', function () {
+        this.style.color = 'black'
+        this.style.backgroundColor = "transparent"
+    })
+})
+
+buttons.forEach((n) => {
+    n.addEventListener('click', function () {
+        this.innerHTML = "Added"
+        this.style.color = "orange"
+    })
 })
